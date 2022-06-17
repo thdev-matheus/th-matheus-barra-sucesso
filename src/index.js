9d1888b485accb0178acfdfb13f2146c9cd4c4c6
th-matheus <matheusth.arte@gmail.com>
2022-06-17 00:14:54 -0300

    Organizando as libs

--- src/index.js
+++ src/index.js
@@ -1,37 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import BarraSucesso from "./BarraSucesso";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter } from "react-router-dom";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#84d4df",
-      light: "#dbfbff",
-      dark: "#2b97a5",
-    },
-    error: {
-      main: "#d69c6d",
-      light: "#f7caa5",
-      dark: "#b1713d",
-    },
-    text: {
-      primary: "#2b97a5",
-      secondary: "#84d4df",
-      disabled: "#84d4df",
-    },
-    divider: "#84d4df",
-  },
-});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <BarraSucesso />
-      </ThemeProvider>
+      <BarraSucesso />
     </BrowserRouter>
   </React.StrictMode>
 );
